946df41b989c91b0e20f48ff416a536937c781a3
MichaelKawwa <kawwamichael@gmail.com>
2025-02-11 10:35:58 -0500

    start screen done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
-import { AppBar, Toolbar, Hourglass, styleReset } from 'react95';
+import { AppBar, Button, Toolbar, MenuList, MenuListItem, Separator, TextInput, Hourglass, styleReset } from 'react95';
 import { createGlobalStyle, ThemeProvider, styled } from 'styled-components';
 
 import peggysPastels from 'react95/dist/themes/peggysPastels';
+
+import logoIMG from './assets/logo.png';
 
 import ms_sans_serif from 'react95/dist/fonts/ms_sans_serif.woff2';
 import ms_sans_serif_bold from 'react95/dist/fonts/ms_sans_serif_bold.woff2';
@@ -29,12 +31,69 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 
-const App = () => (
-  <div className='desktop'>
-    <GlobalStyles />
-    <ThemeProvider theme={peggysPastels}>
-    </ThemeProvider>
-    </div>
-);
+function App() {
+
+  const [open, setOpen] = useState(false);
+
+    return(
+      <div className='desktop' style={{ background: peggysPastels.desktopBackground }}>
+        <GlobalStyles />
+        <ThemeProvider theme={peggysPastels}>
+          <nav>
+            <AppBar style={{ zIndex: 3, padding: '5px' }}>
+              <Toolbar style={{ justifyContent: 'space-between' }}>
+            <div style={{ position: 'relative', display: 'inline-block' }}>
+              <Button
+                onClick={() => setOpen(!open)}
+                active={open}
+                style={{ fontWeight: 'bold' }}
+              >
+                <img
+                  src={logoIMG}
+                  alt='react95 logo'
+                  style={{ height: '20px', marginRight: 4 }}
+                />
+                Start
+              </Button>
+              {open && (
+                <MenuList
+                  style={{
+                    position: 'absolute',
+                    left: '0',
+                    top: '100%'
+                  }}
+                  onClick={() => setOpen(false)}
+                >
+                  <MenuListItem>
+                    <span role='img' aria-label='👨‍💻'>
+                      👨‍💻
+                    </span>
+                     Michael
+                  </MenuListItem>
+                  <MenuListItem>
+                    <span role='img' aria-label='📁'>
+                      📁
+                    </span>
+                    Resume
+                  </MenuListItem>
+                  <Separator />
+                  <MenuListItem disabled>
+                    <span role='img' aria-label='🔙'>
+                      🔙
+                    </span>
+                    Logout
+                  </MenuListItem>
+                </MenuList>
+              )}
+            </div>
+
+            <TextInput placeholder='Search...' width={150} />
+          </Toolbar>
+            </AppBar>
+          </nav>
+        </ThemeProvider>
+        </div>
+    );
+};
 
 export default App;
